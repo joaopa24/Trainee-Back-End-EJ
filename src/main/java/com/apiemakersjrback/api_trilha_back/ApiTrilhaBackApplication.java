79a2009db097c357f311b3dea185942bb75c85b4
joaopa24 <joaokn129@gmail.com>
2024-12-12 00:18:01 -0300

    base created

---
+++ src/main/java/com/apiemakersjrback/api_trilha_back/ApiTrilhaBackApplication.java
@@ -0,0 +1,13 @@
+package com.apiemakersjrback.api_trilha_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrilhaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTrilhaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiemakersjrback/api_trilha_back/ApiTrilhaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiemakersjrback.api_trilha_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTrilhaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
