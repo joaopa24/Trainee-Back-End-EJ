294736a1e50e37a3283d9982f9127f39d5824f57
joaopa24 <joaokn129@gmail.com>
2024-12-16 16:06:51 -0300

    correct db

--- database.sql
+++ database.sql
@@ -1,23 +1,23 @@
-CREATE TABLE "Livro" (
-  "idLivro" INTEGER PRIMARY KEY AUTOINCREMENT, 
-  "nome" VARCHAR(45) NOT NULL,
-  "autor" VARCHAR(45) NOT NULL,
-  "data_de_lancamento" DATE NOT NULL, 
-  "situacao" BOOLEAN
+CREATE TABLE Livro (
+  idLivro SERIAL PRIMARY KEY, 
+  nome VARCHAR(45) NOT NULL,
+  autor VARCHAR(45) NOT NULL,
+  data_de_lancamento DATE NOT NULL, 
+  situacao BOOLEAN
 );
 
-CREATE TABLE "Pessoa" (
-  "idPessoa" INTEGER PRIMARY KEY AUTOINCREMENT, 
-  "nome" VARCHAR(80) NOT NULL,
-  "cep" CHAR(9) NOT NULL,
-  "email" VARCHAR(80) NOT NULL UNIQUE, 
-  "password" VARCHAR(100) NOT NULL
+CREATE TABLE Pessoa (
+  idPessoa SERIAL PRIMARY KEY, 
+  nome VARCHAR(80) NOT NULL,
+  cep CHAR(9) NOT NULL,
+  email VARCHAR(80) NOT NULL UNIQUE, 
+  password VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE "Emprestimo" (
-  "idEmprestimo" INTEGER PRIMARY KEY AUTOINCREMENT, 
-  "idLivro" INTEGER NOT NULL,
-  "idPessoa" INTEGER NOT NULL, 
-  FOREIGN KEY ("idLivro") REFERENCES "Livro" ("idLivro"),
-  FOREIGN KEY ("idPessoa") REFERENCES "Pessoa" ("idPessoa")
+CREATE TABLE Emprestimo (
+  idEmprestimo SERIAL PRIMARY KEY, 
+  idLivro INTEGER NOT NULL,
+  idPessoa INTEGER NOT NULL, 
+  FOREIGN KEY (idLivro) REFERENCES Livro(idLivro),
+  FOREIGN KEY (idPessoa) REFERENCES Pessoa(idPessoa)
 );
